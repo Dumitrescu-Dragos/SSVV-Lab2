8b4ac7ee824651ee3725c2139cc996aa2fdf2081
Dumitrescu Dragos <dumitrescu.dragos.lucian87@gmail.com>
2019-03-13 10:42:43 +0200

    Initial commit

---
+++ src/main/java/LabAssiAsseProjectV02MV/App.java
@@ -0,0 +1,13 @@
+package LabAssiAsseProjectV02MV;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
